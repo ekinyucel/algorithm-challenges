a90a5bb2515ae01a33818941751134a4682cdb74
Broke116 <ekiny018@gmail.com>
2017-01-04 21:34:46 +0300

    c# - depth first search

---
+++ graphs/csharp-graphs/DFS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_graphs
+{
+    public class DFS
+    {
+        private static List<List<int>> _list;
+
+        public DFS(List<List<int>> list)
+        {
+            _list = list;
+        }
+
+        public void Iterative(int s)
+        {
+            var visited = new bool[_list.Count];
+            var stack = new Stack<int>();
+            stack.Push(s);
+
+            while (stack.Count != 0)
+            {
+                var v = stack.Pop();
+                if (!visited[v])
+                {
+                    visited[v] = true;
+                    Console.Write(v + " ");
+                    var secondStack = new Stack<int>();
+                    foreach (var w in _list[v])
+                    {
+                        if (!visited[w])
+                            secondStack.Push(w);
+                    }
+                    while (secondStack.Count != 0)
+                    {
+                        stack.Push(secondStack.Pop());
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- graphs/csharp-graphs/Program.cs
+++ graphs/csharp-graphs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csharp_graphs
 {
@@ -6,6 +7,33 @@ namespace csharp_graphs
     {
         static void Main(string[] args)
         {
+            var list = new List<List<int>>();
+
+            for (var i = 0; i < 7; i++)
+            {
+                list.Add(new List<int>());
+            }
+
+            list[0].Add(1);
+            list[0].Add(2);
+            list[0].Add(3);
+
+            list[1].Add(5);
+            list[1].Add(6);
+
+            list[2].Add(4);
+
+            list[3].Add(2);
+            list[3].Add(4);
+
+            list[4].Add(1);
+
+            list[6].Add(4); 
+
+            var dfs = new DFS(list);
+            dfs.Iterative(0);
+
+            Console.ReadLine();
         }
     }
 }
