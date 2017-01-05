bb44255b1c106cd987d0945846b1df4c5bda51f5
Broke116 <ekiny018@gmail.com>
2017-01-05 22:18:22 +0300

    bfs search

--- graphs/csharp-graphs/BFS.cs
+++ graphs/csharp-graphs/BFS.cs
@@ -21,6 +21,7 @@ namespace csharp_graphs
             _list[v].Add(w);
         }
 
+        #region traverse
         public void Traverse(int s)
         {
             var visited = new bool[_v];
@@ -47,5 +48,36 @@ namespace csharp_graphs
                 }
             }
         }
+        #endregion
+
+        #region search
+        public bool Search(int s, int data)
+        {
+            var visited = new bool[_v];
+
+            for (var i = 0; i < _v; i++)
+                visited[i] = false;
+
+            var queue = new Queue<int>();
+            queue.Enqueue(s);
+            visited[s] = true;
+
+            while (queue.Count != 0)
+            {
+                var v = queue.Dequeue();
+
+                if (v == data) return true;
+
+                foreach (var w in _list[v])
+                {
+                    if (!visited[w])
+                    {
+                        visited[w] = true;
+                        queue.Enqueue(w);
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
